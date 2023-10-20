2120b337655edad248125ba4952ea993ee2d77a8
lizsobolik <113140436+lizsobolik@users.noreply.github.com>
2023-10-20 01:30:22 -0700

    Add files via upload

---
+++ S1_Data/adaptation_R-scripts/adaptation_RAVA_out_to_figure_in.R
@@ -0,0 +1,104 @@
+library(tidyverse)
+library(dplyr)
+
+#########################################################################################################################
+# Define some functions
+#########################################################################################################################
+
+# A function to combine rows in df that represent the same sample (sample name = Passage) 
+coalesce_by_column <- function(df) {
+  return(dplyr::coalesce(!!! as.list(df)))
+}
+
+# A function to compute the symmetric difference (opposite of intersection) between two vectors 
+sym_diff <- function(a,b) unique(c(setdiff(a,b), setdiff(b,a)))
+
+#########################################################################################################################
+# Define some parameters for this analysis 
+#########################################################################################################################
+
+# List of mutations of interest
+lineage_specific_mutations <- c("polymerase_PA M579I",	
+                                "polymerase_PA N222S",	
+                                "polymerase_PA S395N",	
+                                "polymerase_PB1 M290V",
+                                "polymerase_PB1 M339I",	
+                                "polymerase_PB1 T46A",	
+                                "polymerase_PB1 V285I",	
+                                "polymerase_PB2 E180K",	
+                                "polymerase_PB2 E191K",	
+                                "polymerase_PB2 K189R",	
+                                "polymerase_PB2 T491M",	
+                                "polymerase_PB2 Y488C")
+
+# Minimum depth for considering allele frequency at a position 
+min_depth <- 5
+
+#########################################################################################################################
+setwd('/Volumes/lizso_backup_drive/Lieber_Flu_final/Figure-1D_Adaptation/')
+rava.final.path <- paste0(getwd(),"/rava_final_tables_by_lineage/")
+rava.final <- paste0(rava.final.path, list.files(path=rava.final.path, pattern = "*_final.csv")) %>% 
+  map_df(~read_csv(.))
+
+# remove spaces in column names
+names(rava.final)<-str_replace_all(names(rava.final), c(" " = "." , "," = ""))
+
+# get list of all samples
+all_samples <- unique(rava.final$Passage)
+rava.final$Passage<- str_replace_all(rava.final$Passage, c("-rep2" = ""))
+hyb_lin_specific <- rava.final %>% filter(Amino.Acid.Change %in% lineage_specific_mutations)
+
+# list all samples that don't have lineage-specific mutations
+lin_mut_samples <- unique(hyb_lin_specific$Passage)
+no_lin_muts <- sym_diff(all_samples, lin_mut_samples)
+
+# remove "polymerase_" and the spaces from Amino.Acid.Change values for legibility
+hyb_lin_specific$Amino.Acid.Change <- str_replace_all(hyb_lin_specific$Amino.Acid.Change, 
+                                                      c("polymerase_" = "", " " = ":"))
+
+# remove unused columns from the dataframe
+temp <- hyb_lin_specific %>% select(-one_of('Position', 
+                                            'Change', 
+                                            'Protein', 
+                                            'NucleotideChange', 
+                                            'LetterChange',
+                                            'Syn',
+                                            'Sample')) 
+
+# a slightly hacky way to add samples without lineage mutationss to the final table for downstream analysis. Chose 
+# one AA change at random to initialize the rows (PB2:Y488C). The rest will get filled in with after pivot_wider(). 
+temp <- temp %>% add_row(Passage = no_lin_muts, Amino.Acid.Change = "PB2:Y488C")
+
+# calculate the number of reads with the mutant allele
+temp$mut_allele_reads <- temp$AF * temp$Depth / 100
+
+# Sum the depth and number of mutant allele reads across replicate entries (sum the data from first and second sequencing runs)
+temp <- temp %>% group_by(Passage, Amino.Acid.Change) %>% summarise(across(c(mut_allele_reads, Depth), sum))
+
+# Re-calculate the % AF using the summed Depth and number of mutant allele reads
+temp$AF <- temp$mut_allele_reads / temp$Depth * 100
+temp <- temp %>% select(-one_of('mut_allele_reads'))
+
+# Find samples with low depth at loci of interest
+isd <- temp %>% filter(Depth <= min_depth)
+isd <- unique(isd$Passage)
+
+# add columns for each lineage-specific mutation (Rows are filled in with null if data not already there)
+final_hyb_lin_specific <- temp %>% 
+  pivot_wider(names_from = Amino.Acid.Change,
+              values_from = c(AF,Depth))
+
+# Reorder columns so that AF and Depth for each mutation are next to each other and mutations are grouped by segment
+# TODO: would rather this not be hardcoded
+final_hyb_lin_specific <- final_hyb_lin_specific[, c(1,11,23,3,15,13,25,4,16,2,14,7,19,12,24,5,17,8,20,6,18,10,22,9,21)]
+
+# Filter out the samples with insufficient depth, put them in a separate df
+insufficient_depth <- final_hyb_lin_specific %>% filter(Passage %in% isd)
+final_hyb_lin_specific <- final_hyb_lin_specific %>% filter(!(Passage %in% isd))
+
+
+write.csv(final_hyb_lin_specific, file = "adaptation_RAVA_output_to_figure_input/adaptation_summary_lineage_mutations.csv")
+
+write.csv(no_lin_muts, file = "adaptation_RAVA_output_to_figure_input/adaptation_samples_without_lineage_mutations.csv")
+
+write.csv(insufficient_depth, file = "adaptation_RAVA_output_to_figure_input/adaptation_insufficient_depth.csv")
---
+++ S1_Data/adaptation_R-scripts/adaptation_generate_final_figure.R
@@ -0,0 +1,242 @@
+library(tidyverse)
+library(cowplot)
+library(reshape2)
+library(readxl)
+
+setwd('/Volumes/lizso_backup_drive/Lieber_Flu_final/Figure-1D_Adaptation/')
+af_full <- read_csv("adaptation_RAVA_output_to_figure_input/adaptation_summary_lineage_mutations.csv")
+
+samples <- as.data.frame(str_split_fixed(af_full$Passage, "-", 3))
+colnames(samples) <- c("Virus", "PassageNum", "Conc_2749")
+
+af_only <- af_full[,grepl("AF_", colnames(af_full))]
+
+af <- cbind(samples, af_only)
+af <- melt(af)
+af$variable <- gsub("AF_", "", af$variable)
+split <- as.data.frame(str_split_fixed(af$variable, ":", 2))
+af <- cbind(af %>%select("Virus", "PassageNum", "Conc_2749"), split, af %>% select(value))
+colnames(af)[4:6] <- c("Gene", "Mutation", "Allele Frequency")
+
+af$Virus <- paste0("Virus ", af$Virus)
+af[is.na(af)] <- 0
+
+af$PassageNum <- gsub("Inoculum", "Inoc", af$PassageNum)
+
+af$virus_mut <- paste(af$Virus, af$Gene, af$Mutation)
+af$virus_mut <- gsub(" ", "_", af$virus_mut)
+af$`Gene (Mutation)` <- paste0(af$Gene, " (", af$Mutation, ")")
+afsum0 <- af
+########################################################################################################################################
+########################################################################################################################################
+# MAX ALLELE FREQUENCY IS GREATER THAN OR EQUAL TO 50%
+########################################################################################################################################
+########################################################################################################################################
+
+muts <- af %>% group_by(virus_mut) %>% summarize(max=(max(`Allele Frequency`)))
+write_csv(muts, "adaptation_generate_final_figure/adaptation_IAV_muts_all.csv")
+muts <- muts %>% filter(max >= 50) 
+
+write_csv(af, "adaptation_generate_final_figure/adaptation_IAV_af.csv")
+write_csv(af_full, "adaptation_generate_final_figure/adaptation_IAV_af_full.csv")
+write_csv(muts, "adaptation_generate_final_figure/adaptation_IAV_muts_filteredMedian0.csv")
+
+af <- af %>% filter(af$virus_mut %in% muts$virus_mut)
+af <- af %>% mutate(PassageNum = str_replace_all(PassageNum, "P",""))
+af$PassageNum <- factor(af$PassageNum, levels=c('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+
+
+#superscript https://stackoverflow.com/questions/28978011/how-to-subscript-the-x-axis-tick-label
+#fix legend spacing https://stackoverflow.com/questions/11366964/is-there-a-way-to-change-the-spacing-between-legend-items-in-ggplot2
+
+color_codes <- c("PA (N222S)"  = "cornflowerblue",
+                 "PB2 (E180K)" = "red2",
+                 "PB1 (M339I)" = "black",
+                 "PB2 (Y488C)" = "red2",
+                 "PB1 (V285I)" = "black",
+                 "PB1 (M290V)" = "black",
+                 "PA (M579I)"  = "cornflowerblue",
+                 "PB1 (T46A)" = "black",
+                 "PB2 (K189R)" = "red2" ,
+                 "PB2 (E191K)" = "orange",
+                 "PB2 (T491M)" = "orange",
+                 "PA (S395N)" = "cornflowerblue", 
+                 "   " = NA, 
+                 "  " = NA, 
+                 " " = NA)
+shape_codes <- c("PA (N222S)"  = 1,
+                 "PB2 (E180K)" = 2,
+                 "PB1 (M339I)" = 3,
+                 "PB2 (Y488C)" = 4,
+                 "PB1 (V285I)" = 5,
+                 "PB1 (M290V)" = 6,
+                 "PA (M579I)"  = 7,
+                 "PB1 (T46A)" = 8,
+                 "PB2 (K189R)" = 9 ,
+                 "PB2 (E191K)" = 10,
+                 "PB2 (T491M)" = 11,
+                 "PA (S395N)" = 12,
+                 "   " = NA, 
+                 "  " = NA, 
+                 " " = NA)
+#virus 1
+df1 <- af %>% filter(Virus == "Virus 1")
+legend_labels1 <- paste0(df1$`Gene (Mutation)`[df1$PassageNum==max(as.numeric(df1$PassageNum))],", ", df1$`Allele Frequency`[df1$PassageNum==max(as.numeric(df1$PassageNum))], "%")
+df1$PassageNum <- factor(df1$PassageNum, levels=levels(af$PassageNum))
+df1$`Gene (Mutation)` <- factor(df1$`Gene (Mutation)`, levels=c(unique(df1$`Gene (Mutation)`), "   ", "  ", " "))
+A <- ggplot(df1, aes(x=PassageNum, y=`Allele Frequency`, color=`Gene (Mutation)`, group=`Gene (Mutation)`, shape = `Gene (Mutation)`)) + 
+  geom_point(alpha=0.5, size=1.5) + geom_line() +
+  scale_color_manual(values=color_codes, labels = c(legend_labels1, "", "", ""), na.value="white", drop=FALSE, name="") +
+  scale_shape_manual(values=shape_codes, labels = c(legend_labels1, "", "", ""), na.value=1, drop=FALSE, name="")+
+  labs(x="Passage number") +
+  ylab("Allele Frequency (%)") +
+  ggtitle("NGS - Lineage 1") +
+  ylim(c(-1,105)) +
+  theme_classic(base_size=6)+
+  theme(legend.position="bottom", 
+        legend.direction = "vertical",
+        legend.spacing.x = unit(0, 'char'),
+        axis.text.x = element_text(size=6),
+        axis.title.x = element_text(size=4),
+        legend.title = element_blank(),
+        legend.margin = margin(c(-10, 0, 0, 0)),
+        legend.text = element_text(margin = margin(r = 5, unit = "pt")))
+A
+
+
+#virus 2
+df2 <- af %>% filter(Virus == "Virus 2")
+legend_labels2 <- paste0(df2$`Gene (Mutation)`[df2$PassageNum==max(as.numeric(df2$PassageNum))],", ", df2$`Allele Frequency`[df2$PassageNum==max(as.numeric(df2$PassageNum))], "%")
+df2$PassageNum <- factor(df2$PassageNum, levels=levels(af$PassageNum))
+df2$`Gene (Mutation)` <- factor(df2$`Gene (Mutation)`, levels=c(unique(df2$`Gene (Mutation)`), " "))
+B <- ggplot(df2, aes(x=PassageNum, y=`Allele Frequency`, color=`Gene (Mutation)`, group=`Gene (Mutation)`, shape = `Gene (Mutation)`)) + 
+  geom_point(alpha=0.5, size=1.5) + geom_line() +
+  scale_color_manual(values=color_codes, labels = c(legend_labels2, ""), na.value="white", drop=FALSE, name="") +
+  scale_shape_manual(values=shape_codes, labels = c(legend_labels2, ""), na.value=1, drop=FALSE, name="")+
+  labs(x="Passage number") +
+  ylab("Allele Frequency (%)") +
+  ggtitle("NGS - Lineage 2") +
+  ylim(c(-1,105)) +
+  theme_classic(base_size=6)+
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position="bottom", 
+        legend.direction = "vertical",
+        legend.spacing.x = unit(0, 'char'),
+        axis.text.x = element_text(size=6),
+        axis.title.x = element_text(size=4),
+        legend.title = element_blank(),
+        legend.margin = margin(c(-10, 0, 0, 0)),
+        legend.text = element_text(margin = margin(r = 5, unit = "pt")))
+B
+
+#virus 3
+df3 <- af %>% filter(Virus == "Virus 3")
+legend_labels3 <- paste0(df3$`Gene (Mutation)`[df3$PassageNum==max(as.numeric(df3$PassageNum))],", ", df3$`Allele Frequency`[df3$PassageNum==max(as.numeric(df3$PassageNum))], "%")
+df3$PassageNum <- factor(df3$PassageNum, levels=levels(af$PassageNum))
+df3$`Gene (Mutation)` <- factor(df3$`Gene (Mutation)`, levels=c(unique(df3$`Gene (Mutation)`), " ", "  "))
+C <- ggplot(df3, aes(x=PassageNum, y=`Allele Frequency`, color=`Gene (Mutation)`, group=`Gene (Mutation)`, shape = `Gene (Mutation)`)) + 
+  geom_point(alpha=0.5, size=1.5) + geom_line() +
+  scale_color_manual(values=color_codes, labels = c(legend_labels3, "", ""), na.value="white", drop=FALSE, name="") +
+  scale_shape_manual(values=shape_codes, labels = c(legend_labels3, "", ""), na.value=1, drop=FALSE, name="")+
+  labs(x="Passage number") +
+  ylab("Allele Frequency (%)") +
+  ggtitle("NGS - Lineage 3") +
+  ylim(c(-1,105)) +
+  theme_classic(base_size=6)+
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position="bottom", 
+        legend.direction = "vertical",
+        legend.spacing.x = unit(0, 'char'),
+        axis.text.x = element_text(size=6),
+        axis.title.x = element_text(size=4),
+        legend.title = element_blank(),
+        legend.margin = margin(c(-10, 0, 0, 0)),
+        legend.text = element_text(margin = margin(r = 5, unit = "pt")))
+C
+
+#virus 4
+df4 <- af %>% filter(Virus == "Virus 4")
+legend_labels4 <- paste0(df4$`Gene (Mutation)`[df4$PassageNum==max(as.numeric(df4$PassageNum))],", ", df4$`Allele Frequency`[df4$PassageNum==max(as.numeric(df4$PassageNum))], "%")
+df4$PassageNum <- factor(df4$PassageNum, levels=levels(af$PassageNum))
+df4$`Gene (Mutation)` <- factor(df4$`Gene (Mutation)`, levels=c(unique(df4$`Gene (Mutation)`), " "))
+D <- ggplot(df4, aes(x=PassageNum, y=`Allele Frequency`, color=`Gene (Mutation)`, group=`Gene (Mutation)`, shape = `Gene (Mutation)`)) + 
+  geom_point(alpha=0.5, size=1.5) + geom_line() +
+  scale_color_manual(values=color_codes, labels = c(legend_labels4, ""), na.value="white", drop=FALSE, name="") +
+  scale_shape_manual(values=shape_codes, labels = c(legend_labels4, ""), na.value=1, drop=FALSE, name="")+
+  labs(x="Passage number") +
+  ylab("Allele Frequency (%)") +
+  ggtitle("NGS - Lineage 4") +
+  ylim(c(-1,105)) +
+  theme_classic(base_size=6)+
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position="bottom", 
+        legend.direction = "vertical",
+        legend.spacing.x = unit(0, 'char'),
+        axis.text.x = element_text(size=6),
+        axis.title.x = element_text(size=4),
+        legend.title = element_blank(),
+        legend.margin = margin(c(-10, 0, 0, 0)),
+        legend.text = element_text(margin = margin(r = 5, unit = "pt")))
+D
+
+#virus 5
+df5 <- af %>% filter(Virus == "Virus 5")
+legend_labels5 <- paste0(df5$`Gene (Mutation)`[df5$PassageNum==max(as.numeric(df5$PassageNum))],", ", df5$`Allele Frequency`[df5$PassageNum==max(as.numeric(df5$PassageNum))], "%")
+df5$PassageNum <- factor(df5$PassageNum, levels=levels(af$PassageNum))
+df5$`Gene (Mutation)` <- factor(df5$`Gene (Mutation)`, levels=c(unique(df5$`Gene (Mutation)`), " ", "  "))
+E <- ggplot(df5, aes(x=PassageNum, y=`Allele Frequency`, color=`Gene (Mutation)`, group=`Gene (Mutation)`, shape = `Gene (Mutation)`)) + 
+  geom_point(alpha=0.5, size=1.5) + geom_line() +
+  scale_color_manual(values=color_codes, labels = c(legend_labels5, "", ""), na.value="white", drop=FALSE, name="") +
+  scale_shape_manual(values=shape_codes, labels = c(legend_labels5, "", ""), na.value=1, drop=FALSE, name="")+
+  labs(x="Passage number") +
+  ylab("Allele Frequency (%)") +
+  ggtitle("NGS - Lineage 5") +
+  ylim(c(-1,105)) +
+  theme_classic(base_size=6)+
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position="bottom", 
+        legend.direction = "vertical",
+        legend.spacing.x = unit(0, 'char'),
+        axis.text.x = element_text(size=6),
+        axis.title.x = element_text(size=4),
+        legend.title = element_blank(),
+        legend.margin = margin(c(-10, 0, 0, 0)),
+        legend.text = element_text(margin = margin(r = 5, unit = "pt")))
+E
+
+#virus 6
+df6 <- af %>% filter(Virus == "Virus 6")
+legend_labels6 <- paste0(df6$`Gene (Mutation)`[df6$PassageNum==max(as.numeric(df6$PassageNum))],", ", df6$`Allele Frequency`[df6$PassageNum==max(as.numeric(df6$PassageNum))], "%")
+df6$PassageNum <- factor(df6$PassageNum, levels=levels(af$PassageNum))
+df6$`Gene (Mutation)` <- factor(df6$`Gene (Mutation)`, levels=c(unique(df6$`Gene (Mutation)`), " "))
+
+f <- ggplot(df6, aes(x=PassageNum, y=`Allele Frequency`, color=`Gene (Mutation)`, group=`Gene (Mutation)`, shape = `Gene (Mutation)`)) + 
+  geom_point(alpha=0.5, size=1.5) + geom_line() +
+  scale_color_manual(values=color_codes, labels = c(legend_labels6, ""), na.value="white", drop=FALSE, name="") +
+  scale_shape_manual(values=shape_codes, labels = c(legend_labels6, ""), na.value=1, drop=FALSE, name="")+
+  labs(x="Passage number") +
+  ylab("Allele Frequency (%)") +
+  ggtitle("NGS - Lineage 6") +
+  ylim(c(-1,105)) +
+  theme_classic(base_size=6) +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        legend.position="bottom", 
+        legend.direction = "vertical",
+        legend.spacing.x = unit(0, 'char'),
+        axis.text.x = element_text(size=6),
+        axis.title.x = element_text(size=4),
+        legend.title = element_blank(),
+        legend.margin = margin(c(-10, 0, 0, 0)),
+        legend.text = element_text(margin = margin(r = 5, unit = "pt")))
+f
+
+cow <- plot_grid(A, B, C, D, E, f, ncol=6, rel_widths = c(1.2,1,1,1,1,1))
+
+pdf(file="adaptation_generate_final_figure/Fig1D_adaptation_mNGS_threshold50.pdf", width=6.5, height=2)
+cow
+dev.off()
